ec5c36fa8735cad08dcdc69c0de27da049f524d2
Gerard <gerardllamoso@gmail.com>
2021-07-31 07:40:01 -0700

    Made constructor private for ExpenseViewHolder.kt and added "create" method

--- app/src/main/java/dev/gtcl/travelbank/presentation/expense/overview/adapter/ExpenseViewHolder.kt
+++ app/src/main/java/dev/gtcl/travelbank/presentation/expense/overview/adapter/ExpenseViewHolder.kt
@@ -1,18 +1,25 @@
 package dev.gtcl.travelbank.presentation.expense.overview.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import dev.gtcl.travelbank.databinding.ItemExpenseBinding
 import dev.gtcl.travelbank.domain.model.Expense
 
-class ExpenseViewHolder(
+class ExpenseViewHolder private constructor(
     private val binding: ItemExpenseBinding
     ): RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(expense: Expense, expenseClickListener: ExpenseClickListener){
-            binding.expense = expense
-            binding.cardView.setOnClickListener {
-                expenseClickListener.onClick(expense)
-            }
-            binding.invalidateAll()
+    fun bind(expense: Expense, expenseClickListener: ExpenseClickListener){
+        binding.expense = expense
+        binding.cardView.setOnClickListener {
+            expenseClickListener.onClick(expense)
         }
+        binding.invalidateAll()
+    }
+
+    companion object {
+        fun create(parent: ViewGroup) =
+            ExpenseViewHolder(ItemExpenseBinding.inflate(LayoutInflater.from(parent.context)))
+    }
 }
