7786d2a7f00a51d59493777bffa61694b7dc8b3e
Gerard <gerardllamoso@gmail.com>
2021-07-30 21:30:19 -0700

    Changed date to show single digit for day number

--- app/src/main/java/dev/gtcl/travelbank/Util.kt
+++ app/src/main/java/dev/gtcl/travelbank/Util.kt
@@ -7,8 +7,8 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 val REMOTE_DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US)
-val MONTH_DAY_FORMAT = SimpleDateFormat("MMM dd", Locale.US)
-val MONTH_DAY_YEAR_FORMAT = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+val MONTH_DAY_FORMAT = SimpleDateFormat("MMM d", Locale.US)
+val MONTH_DAY_YEAR_FORMAT = SimpleDateFormat("MMM d, yyyy", Locale.US)
 
 fun convertRemoteDateToMonthDayAndYear(remoteDate: String): String{
     val date = REMOTE_DATE_FORMAT.parse(remoteDate)!!
