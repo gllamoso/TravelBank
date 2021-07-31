91ab0212b2d3d2b35eee26b48e5a44610ea26a99
Gerard <gerardllamoso@gmail.com>
2021-07-30 21:38:40 -0700

    Removed unused imports in TestUtils.kt

--- app/src/androidTest/java/dev/gtcl/travelbank/utils/TestUtils.kt
+++ app/src/androidTest/java/dev/gtcl/travelbank/utils/TestUtils.kt
@@ -1,11 +1,6 @@
 package dev.gtcl.travelbank.utils
 
-import android.view.View
 import androidx.annotation.IdRes
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.matcher.BoundedMatcher
-import org.hamcrest.Description
-import org.hamcrest.Matcher
 
 fun withRecyclerView(@IdRes recyclerViewId: Int) =
     RecyclerViewMatcher(recyclerViewId)
